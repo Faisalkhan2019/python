9faa44d1c021f5dc4c7847c79819c4167a3bba47
Muhammad Faisal Khan <49272303+Faisalkhan2019@users.noreply.github.com>
2026-02-05 17:35:03 +0500

    Variables

---
+++ variables.py
@@ -0,0 +1,47 @@
+
+# name = str(input("Enter your name: "))
+# age = int(input("Enter your age: "))
+# school = "Oxford"
+# is_student = True
+
+# print(f"Your name is: {name}")
+# print("--------------------")
+# print(f"Your age is: {age}")
+# print("--------------------")
+# print(f"Your school is: {school}")
+# print("--------------------")
+# print(f"Are you Student: {is_student}")
+
+# def main(a, b):
+#     return a + b
+
+# print(main(5, 10))
+    
+# print ("Hello world")
+
+# mix text and numbers
+
+# name = str(input("Enter Your Name "))
+# age = int(input("Enter Your Age: $"))
+# print(f"Hi {name} your age is {age}")
+
+# print("My name is Faisal", 30, "Age")
+
+# ----- Math Operators ----- 
+# print(35%6)
+# print(5 ** 5)
+# print( 5/2)
+
+x = 4 # this is int
+y = "Faisal" # this is str
+z = True # this is bool 
+
+# print(3.0-1) # Output: 2.0 
+
+
+print(f"3 + 2 = {3 + 2}")
+# print(f"5 – 1 = {5 – 1}")
+print(f"3 * 4 = {3 * 4}")
+print(f"10 / 5 = {10 / 5}")
+print(f"35 % 3 = {35 % 3}")
+print(f"2 ** 4 = {2 ** 4}")
